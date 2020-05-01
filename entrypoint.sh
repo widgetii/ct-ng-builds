cf537f3c2e532370bf8b455f6ca3f583c1da123c
Dmitry Ilyin <dima@doty.ru>
2020-05-01 12:58:32 +0300

    Test release

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,11 @@ fi
 cd $HOME/crosstools-ng
 ./ct-ng show-${TOOLCHAIN}
 ./ct-ng ${TOOLCHAIN}
+
+# don't produce verbose output for CI platform
+sed 's/CT_LOG_PROGRESS_BAR/# CT_LOG_PROGRESS_BAR/' -i .config
+
 ./ct-ng build
+
+cd ~/x-tools
+tar cfJ ${TOOLCHAIN}.tar.xz ${TOOLCHAIN}/*
